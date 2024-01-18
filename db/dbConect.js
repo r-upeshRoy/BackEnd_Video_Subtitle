8d14a92686435b47e77191a213c1405ae2a69577
Roy-Rupesh <rupesh.r@wathare.com>
2024-01-18 12:12:52 +0530

    dbConect.js

--- db/dbConect.js
+++ db/dbConect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 exports.dbConnection = async () => {
     mongoose.set('strictQuery', true)
-    await mongoose.connect(process.env.MONGO_URL)
+    await mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => {
             console.log('Db Connected')
         }).catch((err) => {
